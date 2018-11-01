6ba3c75a71ac09bdb339271e10202c4cd2261bfb
Patrina Bailey <p.t.bailey04@gmail.com>
2018-11-01 13:07:37 -0400

    done

--- create.sql
+++ create.sql
@@ -1,1 +1,9 @@
-CREATE TABLE dogs (id INTEGER PRIMARY KEY, name TEXT, age INTEGER, gender CHAR(1), breed TEXT, temperament TEXT, hungry BOOLEAN);
+
+CREATE TABLE dogs (
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  age INTEGER,
+  gender CHAR(1),
+  breed TEXT,
+  temperament TEXT,
+  hungry BOOLEAN);
